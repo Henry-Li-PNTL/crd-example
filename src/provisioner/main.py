e502a789e77e965bc055746215a232b368b10da6
haowei <henryli@haoweideiMac.local>
2023-06-20 10:16:03 +0800

    feat: Add kopf on update event handler

--- src/provisioner/main.py
+++ src/provisioner/main.py
@@ -1,7 +1,7 @@
-from os import path as os_path
 from typing import Any, Mapping
 import markdown
 import os
+import aiofiles
 
 import kopf
 
@@ -19,11 +19,11 @@ async def create_event_trigger(
     if not name or not namespace:
         return {"status_code": 400, "message": f"Invalid name or namespace: {namespace}/{name}"}
 
-    logger.info(f"receive eventtriggers {namespace}/{name} spec={spec}")
+    logger.info(f"receive markdowns {namespace}/{name} spec={spec}")
 
-    with open(f"archive/{namespace}-{name}.html", "w", encoding="utf-8") as f:
-        f.write(markdown.markdown(spec["content"]))
-
+    async with aiofiles.open(f"archive/{namespace}-{name}.html", "w", encoding="utf-8") as f:
+        await f.write(markdown.markdown(spec["content"]))
+        
     return {"status_code": 200}
 
 
@@ -34,8 +34,23 @@ async def delete_event_trigger(
     if not name or not namespace:
         return {"status_code": 400, "message": f"Invalid name or namespace: {namespace}/{name}"}
 
-    logger.info(f"receive eventtriggers delete {namespace}/{name}")
+    logger.info(f"receive markdowns delete {namespace}/{name}")
 
     os.remove(f"archive/{namespace}-{name}.html")
 
+
+    return {"status_code": 200}
+
+@kopf.on.update("markdowns", retries=3, backoff=30.0)
+async def update_event_trigger(
+    logger: kopf.Logger, spec: Mapping, name: str | None, namespace: str | None, **kwargs: Any
+):
+    if not name or not namespace:
+        return {"status_code": 400, "message": f"Invalid name or namespace: {namespace}/{name}"}
+
+    logger.info(f"receive markdowns uodate {namespace}/{name}")
+
+    async with aiofiles.open(f"archive/{namespace}-{name}.html", "w", encoding="utf-8") as f:
+        await f.write(markdown.markdown(spec["content"]))
+        
     return {"status_code": 200}
